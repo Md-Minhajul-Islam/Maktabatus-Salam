672fd209ac06e5d7978764b65b451c9f880e9c26
Minhaj <minhajulislammuhammad@gmail.com>
2025-10-28 20:57:59 +0600

    c

--- frontend/src/utils/CheckLoggedIn.jsx
+++ frontend/src/utils/CheckLoggedIn.jsx
@@ -2,14 +2,12 @@ import axios from "axios";
 import API_BASE_URL from "../config";
 
 export default async function CheckLoggedIn() {
-  return true;
-  // try {
-  //   const response = await axios.get(`${API_BASE_URL}/check`, {
-  //     withCredentials: true,
-  //   });
-  //   if (response.data.loggedIn) return true;
-  //   // else return false;
-  // } catch (err) {
-  //   return false;
-  // }
+  try {
+    const response = await axios.get(`${API_BASE_URL}/check`, {
+      withCredentials: true,
+    });
+    return true;
+  } catch (err) {
+    return false;
+  }
 }
