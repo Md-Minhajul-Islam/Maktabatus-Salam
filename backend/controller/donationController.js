609be3063971fec9837fdcaef876cf867584be74
Minhaj <minhajulislammuhammad@gmail.com>
2025-11-04 09:48:22 +0600

    testing donationController.js

--- backend/controller/donationController.js
+++ backend/controller/donationController.js
@@ -86,7 +86,8 @@ async function success(req, res, next) {
 
       const response = await sendEmail(donator, subject, text);
     }
-    res.redirect(`${process.env.DONATION_URL_GET}/${tran_id}`);
+    res.redirect("https://maktabatus-salam.vercel.app");
+    // res.redirect(`${process.env.DONATION_URL_GET}/${tran_id}`);
   } catch (err) {
     next(err);
   }
